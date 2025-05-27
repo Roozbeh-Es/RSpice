0f0a0536cdfed3a164516add67fc0d4aa3e60eaa
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-05-27 14:57:47 +0330

    minimal additions

--- include/Circuit .h
+++ include/Circuit .h
@@ -7,25 +7,43 @@
 #include <cmath>
 #include <unordered_map>
 #include <memory>
+#include "Vector.h"
+#include "Matrix.h"
+#include "Resistor.h"
+#include "Capacitor.h"
+#include "Inductor.h"
+#include "AbstractCurrentSource.h"
+#include "AbstractVoltageSource.h"
+#include "SinusiodalVoltageSource.h"
+#include "SinusoidalCurrentSource.h"
+#include "DCCurrentSource.h"
+#include "DCVoltageSource.h"
+
+enum class AnalysisType {
+    DC,
+    TRANSIENT,
+    AC_SWEEP,
+};
 
 class Circuit {
 public:
     Circuit();
+
     ~Circuit();
-    void addElement(std::unique_ptr<Element> element);
-    void deleteElement(std::unique_ptr<Element> element);
-    void renameNode();
-    void displayNodes();
-    void buildMatrix();
+
+    void loadComponents(std::vector<std::unique_ptr<Element> > CircuitComponents, const std::map<std::string, int>& initialNodeMap );
+
+    void buildMNAMatrix(Matrix& G, Matrix& B, Matrix& C, Matrix& D, Vector& J, Vector& E, double timeStep = 0.0);
 
 private:
-    std::vector<std::unique_ptr<Element>> elements;
+    std::vector<std::unique_ptr<Element> > elements;
     std::map<std::string, int> nodeNameToIndex;
     int numNodes_;
     int numVoltageSources_;
     double time_;
     double timeStep_;
-
+    Vector currentNodeVoltages_;
+    Vector CurrentSourceCurrents_;
 };
 
 
