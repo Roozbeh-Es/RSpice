bbecb2efbcedaf9eb87f17cdcae44faf6b517d19
Roozbeh-Es <rouzbehesmaeilpoor1@gmail.com>
2025-05-27 14:32:26 +0330

    completed the current sources for now

---
+++ include/SinusoidalCurrentSource.h
@@ -0,0 +1,28 @@
+#ifndef SINUSOIDALCURRENTSOURCE_H
+#define SINUSOIDALCURRENTSOURCE_H
+
+#include "AbstractCurrentSource.h"
+
+class SinusoidalCurrentSource : public AbstractCurrentSource {
+private:
+    double amplitude_;
+    double frequency_;
+    double offset_;
+
+public:
+    SinusoidalCurrentSource();
+
+    SinusoidalCurrentSource(double amplitude, double frequency, double offset);
+
+    void stamp() override;
+
+    bool isLinear() override {
+        return false;
+    }
+
+    std::string getType() const override {
+        return "Sinusoidal Current Source";
+    }
+};
+
+#endif //SINUSOIDALCURRENTSOURCE_H
